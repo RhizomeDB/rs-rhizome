a41a6cf6ca77ef0b1f429fccddbd10849084e33d
github-action-benchmark <github@users.noreply.github.com>
2022-12-05 23:04:30 +0000

    add Rust Benchmark (cargo) benchmark result for cf550e988a16e5adb5be5c4fbbc99b52a4067ad7

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1670281470249,
+  "repoUrl": "https://github.com/fission-codes/rs-rhizome",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "quinn@quinnwilton.com",
+            "name": "Quinn Wilton",
+            "username": "QuinnWilton"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cf550e988a16e5adb5be5c4fbbc99b52a4067ad7",
+          "message": "Merge pull request #3 from fission-codes/flakes-chromedriver\n\nchore: add chromedriver to flake.nix",
+          "timestamp": "2022-12-05T15:03:39-08:00",
+          "tree_id": "08906c0295f128c0b1ddabcd7ceac8763dbf2a05",
+          "url": "https://github.com/fission-codes/rs-rhizome/commit/cf550e988a16e5adb5be5c4fbbc99b52a4067ad7"
+        },
+        "date": 1670281469064,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "add",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
