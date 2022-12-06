9696ece34794abe01da21db7bd621d7e3917d54e
github-action-benchmark <github@users.noreply.github.com>
2022-12-06 05:01:32 +0000

    add Rust Benchmark (cargo) benchmark result for cb02ae876115ed2b5ff4ebd0a8742d24a4194a40

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670281470249,
+  "lastUpdate": 1670302892445,
   "repoUrl": "https://github.com/fission-codes/rs-rhizome",
   "entries": {
     "Rust Benchmark": [
@@ -23,6 +23,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/fission-codes/rs-rhizome/commit/cf550e988a16e5adb5be5c4fbbc99b52a4067ad7"
         },
         "date": 1670281469064,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "add",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "quinn@quinnwilton.com",
+            "name": "Quinn Wilton",
+            "username": "QuinnWilton"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cb02ae876115ed2b5ff4ebd0a8742d24a4194a40",
+          "message": "Merge pull request #4 from fission-codes/rhizome-wasm-license\n\nchore: add rhizome-wasm/LICENSE",
+          "timestamp": "2022-12-05T21:00:58-08:00",
+          "tree_id": "d64ce7fec1313d644d2352e642303533772af65d",
+          "url": "https://github.com/fission-codes/rs-rhizome/commit/cb02ae876115ed2b5ff4ebd0a8742d24a4194a40"
+        },
+        "date": 1670302891606,
         "tool": "cargo",
         "benches": [
           {
