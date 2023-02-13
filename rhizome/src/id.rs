8b9b07c4d3e98abd418cf1b1033c16be0df298b8
Quinn Wilton <quinn@quinnwilton.com>
2023-02-13 13:02:50 -0800

    refactor: generate ID types using a macro

--- rhizome/src/id.rs
+++ rhizome/src/id.rs
@@ -2,85 +2,37 @@ use std::fmt::Display;
 
 use crate::interner::{self, Symbol};
 
-#[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct AttributeId(Symbol);
+macro_rules! new_id {
+    ($name:ident) => {
+        #[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+        pub struct $name(Symbol);
 
-impl AttributeId {
-    pub fn new(id: &str) -> Self {
-        let symbol = interner::get_or_intern(id);
+        impl $name {
+            pub fn new<T: AsRef<str>>(id: T) -> Self {
+                let symbol = interner::get_or_intern(id.as_ref());
 
-        Self(symbol)
-    }
+                Self(symbol)
+            }
 
-    pub fn resolve(&self) -> String {
-        interner::resolve(self.0)
-    }
+            pub fn resolve(&self) -> String {
+                interner::resolve(self.0)
+            }
+        }
+
+        impl Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.resolve())
+            }
+        }
+
+        impl From<&str> for $name {
+            fn from(id: &str) -> Self {
+                Self::new(id)
+            }
+        }
+    };
 }
 
-impl Display for AttributeId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.resolve())
-    }
-}
-
-impl From<&str> for AttributeId {
-    fn from(id: &str) -> Self {
-        Self::new(id)
-    }
-}
-
-#[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-#[repr(transparent)]
-pub struct RelationId(Symbol);
-
-impl RelationId {
-    pub fn new(id: &str) -> Self {
-        let symbol = interner::get_or_intern(id);
-
-        Self(symbol)
-    }
-
-    pub fn resolve(&self) -> String {
-        interner::resolve(self.0)
-    }
-}
-
-impl Display for RelationId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.resolve())
-    }
-}
-
-impl From<&str> for RelationId {
-    fn from(id: &str) -> Self {
-        Self::new(id)
-    }
-}
-
-#[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-#[repr(transparent)]
-pub struct VariableId(Symbol);
-
-impl VariableId {
-    pub fn new(id: &str) -> Self {
-        let symbol = interner::get_or_intern(id);
-
-        Self(symbol)
-    }
-
-    pub fn resolve(&self) -> String {
-        interner::resolve(self.0)
-    }
-}
-
-impl Display for VariableId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.resolve())
-    }
-}
-
-impl From<&str> for VariableId {
-    fn from(id: &str) -> Self {
-        Self::new(id)
-    }
-}
+new_id!(AttributeId);
+new_id!(RelationId);
+new_id!(VariableId);
