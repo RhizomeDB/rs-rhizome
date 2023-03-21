0d1fd928fb869b2b10bf37b159e81517dcbda5d2
Quinn Wilton <quinn@quinnwilton.com>
2023-03-21 16:22:57 -0700

    refactor: use macros to implement IntoTupleArgs

--- rhizome/src/logic/builder/into_tuple_args.rs
+++ rhizome/src/logic/builder/into_tuple_args.rs
@@ -4,84 +4,30 @@ use crate::{
 };
 
 pub trait VarRefTuple<O> {
-    type Target: IntoTupleArgs<O>;
+    type Target;
+
     fn deref(&self) -> Self::Target;
 }
 
-impl<O> VarRefTuple<O> for () {
-    type Target = ();
+macro_rules! impl_var_ref_tuple {
+    ($($Ts:expr),*) => {
+        paste::item! {
+            impl<'a, O, $([< V $Ts >],)*> VarRefTuple<O> for ($(&'a TypedVar<[< V $Ts >]>,)*)
+            where
+                O: Clone,
+                $(
+                    Type: FromType<[< V $Ts >]>,
+                    [< V $Ts >]: Copy + TryFrom<O, Error = &'static str>,
+                )*
+            {
+                type Target = ($(TypedVar<[< V $Ts >]>,)*);
 
-    fn deref(&self) -> Self::Target {}
-}
-
-impl<'a, O, V0> VarRefTuple<O> for (&'a TypedVar<V0>,)
-where
-    O: Clone,
-    Type: FromType<V0>,
-    V0: Copy + TryFrom<O, Error = &'static str>,
-{
-    type Target = (TypedVar<V0>,);
-
-    fn deref(&self) -> Self::Target {
-        (*self.0,)
-    }
-}
-
-impl<'a, O, V0, V1> VarRefTuple<O> for (&'a TypedVar<V0>, &'a TypedVar<V1>)
-where
-    O: Clone,
-    Type: FromType<V0>,
-    Type: FromType<V1>,
-    V0: Copy + TryFrom<O, Error = &'static str>,
-    V1: Copy + TryFrom<O, Error = &'static str>,
-{
-    type Target = (TypedVar<V0>, TypedVar<V1>);
-
-    fn deref(&self) -> Self::Target {
-        (*self.0, *self.1)
-    }
-}
-
-impl<'a, O, V0, V1, V2> VarRefTuple<O> for (&'a TypedVar<V0>, &'a TypedVar<V1>, &'a TypedVar<V2>)
-where
-    O: Clone,
-    Type: FromType<V0>,
-    Type: FromType<V1>,
-    Type: FromType<V2>,
-    V0: Copy + TryFrom<O, Error = &'static str>,
-    V1: Copy + TryFrom<O, Error = &'static str>,
-    V2: Copy + TryFrom<O, Error = &'static str>,
-{
-    type Target = (TypedVar<V0>, TypedVar<V1>, TypedVar<V2>);
-
-    fn deref(&self) -> Self::Target {
-        (*self.0, *self.1, *self.2)
-    }
-}
-
-impl<'a, O, V0, V1, V2, V3> VarRefTuple<O>
-    for (
-        &'a TypedVar<V0>,
-        &'a TypedVar<V1>,
-        &'a TypedVar<V2>,
-        &'a TypedVar<V3>,
-    )
-where
-    O: Clone,
-    Type: FromType<V0>,
-    Type: FromType<V1>,
-    Type: FromType<V2>,
-    Type: FromType<V3>,
-    V0: Copy + TryFrom<O, Error = &'static str>,
-    V1: Copy + TryFrom<O, Error = &'static str>,
-    V2: Copy + TryFrom<O, Error = &'static str>,
-    V3: Copy + TryFrom<O, Error = &'static str>,
-{
-    type Target = (TypedVar<V0>, TypedVar<V1>, TypedVar<V2>, TypedVar<V3>);
-
-    fn deref(&self) -> Self::Target {
-        (*self.0, *self.1, *self.2, *self.3)
-    }
+                fn deref(&self) -> Self::Target {
+                    ($(*self.$Ts,)*)
+                }
+            }
+        }
+    };
 }
 
 pub trait IntoTupleArgs<O> {
@@ -91,158 +37,52 @@ pub trait IntoTupleArgs<O> {
     fn into_tuple_args(&self, bindings: Vec<O>) -> Self::Output;
 }
 
-impl<O> IntoTupleArgs<O> for () {
-    type Output = ();
+macro_rules! impl_into_tuple_args {
+    ($($Ts:expr),*) => {
+        paste::item! {
+            impl<O, $([< V $Ts >],)*> IntoTupleArgs<O> for ($(TypedVar<[< V $Ts >]>,)*)
+            where
+                O: Clone,
+                $(
+                    Type: FromType<[< V $Ts >]>,
+                    [< V $Ts >]: Copy + TryFrom<O, Error = &'static str>,
+                )*
+            {
+                type Output = ($([< V $Ts >],)*);
 
-    fn into_vars(&self) -> Vec<Var> {
-        vec![]
-    }
+                fn into_vars(&self) -> Vec<Var> {
+                     vec![$(self.$Ts.into(),)*]
+                }
 
-    fn into_tuple_args(&self, bindings: Vec<O>) -> Self::Output {
-        assert!(bindings.is_empty());
-    }
+                #[allow(unused_variables)]
+                fn into_tuple_args(&self, bindings: Vec<O>) -> Self::Output {
+                    (
+                        $(
+                           [< V $Ts >]::try_from(bindings[$Ts].clone()).unwrap(),
+                        )*
+                    )
+                }
+            }
+        }
+    };
 }
 
-impl<V0, O> IntoTupleArgs<O> for (TypedVar<V0>,)
-where
-    O: Clone,
-    Type: FromType<V0>,
-    V0: Copy + TryFrom<O, Error = &'static str>,
-{
-    type Output = (V0,);
+impl_into_tuple_args!();
+impl_into_tuple_args!(0);
+impl_into_tuple_args!(0, 1);
+impl_into_tuple_args!(0, 1, 2);
+impl_into_tuple_args!(0, 1, 2, 3);
+impl_into_tuple_args!(0, 1, 2, 3, 4);
+impl_into_tuple_args!(0, 1, 2, 3, 4, 5);
+impl_into_tuple_args!(0, 1, 2, 3, 4, 5, 6);
+impl_into_tuple_args!(0, 1, 2, 3, 4, 5, 6, 7);
 
-    fn into_vars(&self) -> Vec<Var> {
-        vec![self.0.into()]
-    }
-
-    fn into_tuple_args(&self, bindings: Vec<O>) -> Self::Output {
-        assert!(bindings.len() == 1);
-
-        (V0::try_from(bindings[0].clone()).unwrap(),)
-    }
-}
-
-impl<V0, V1, O> IntoTupleArgs<O> for (TypedVar<V0>, TypedVar<V1>)
-where
-    O: Clone,
-    Type: FromType<V0>,
-    Type: FromType<V1>,
-    V0: Copy + TryFrom<O, Error = &'static str>,
-    V1: Copy + TryFrom<O, Error = &'static str>,
-{
-    type Output = (V0, V1);
-    fn into_vars(&self) -> Vec<Var> {
-        vec![self.0.into(), self.1.into()]
-    }
-
-    fn into_tuple_args(&self, bindings: Vec<O>) -> Self::Output {
-        assert!(bindings.len() == 2);
-
-        (
-            V0::try_from(bindings[0].clone()).unwrap(),
-            V1::try_from(bindings[1].clone()).unwrap(),
-        )
-    }
-}
-impl<V0, V1, V2, O> IntoTupleArgs<O> for (TypedVar<V0>, TypedVar<V1>, TypedVar<V2>)
-where
-    O: Clone,
-    Type: FromType<V0>,
-    Type: FromType<V1>,
-    Type: FromType<V2>,
-    V0: Copy + TryFrom<O, Error = &'static str>,
-    V1: Copy + TryFrom<O, Error = &'static str>,
-    V2: Copy + TryFrom<O, Error = &'static str>,
-{
-    type Output = (V0, V1, V2);
-
-    fn into_vars(&self) -> Vec<Var> {
-        vec![self.0.into(), self.1.into(), self.2.into()]
-    }
-
-    fn into_tuple_args(&self, bindings: Vec<O>) -> Self::Output {
-        assert!(bindings.len() == 3);
-
-        (
-            V0::try_from(bindings[0].clone()).unwrap(),
-            V1::try_from(bindings[1].clone()).unwrap(),
-            V2::try_from(bindings[2].clone()).unwrap(),
-        )
-    }
-}
-
-impl<V0, V1, V2, V3, O> IntoTupleArgs<O>
-    for (TypedVar<V0>, TypedVar<V1>, TypedVar<V2>, TypedVar<V3>)
-where
-    O: Clone,
-    Type: FromType<V0>,
-    Type: FromType<V1>,
-    Type: FromType<V2>,
-    Type: FromType<V3>,
-    V0: Copy + TryFrom<O, Error = &'static str>,
-    V1: Copy + TryFrom<O, Error = &'static str>,
-    V2: Copy + TryFrom<O, Error = &'static str>,
-    V3: Copy + TryFrom<O, Error = &'static str>,
-{
-    type Output = (V0, V1, V2, V3);
-
-    fn into_vars(&self) -> Vec<Var> {
-        vec![self.0.into(), self.1.into(), self.2.into(), self.3.into()]
-    }
-
-    fn into_tuple_args(&self, bindings: Vec<O>) -> Self::Output {
-        assert!(bindings.len() == 4);
-
-        (
-            V0::try_from(bindings[0].clone()).unwrap(),
-            V1::try_from(bindings[1].clone()).unwrap(),
-            V2::try_from(bindings[2].clone()).unwrap(),
-            V3::try_from(bindings[3].clone()).unwrap(),
-        )
-    }
-}
-
-impl<V0, V1, V2, V3, V4, O> IntoTupleArgs<O>
-    for (
-        TypedVar<V0>,
-        TypedVar<V1>,
-        TypedVar<V2>,
-        TypedVar<V3>,
-        TypedVar<V4>,
-    )
-where
-    O: Clone,
-    Type: FromType<V0>,
-    Type: FromType<V1>,
-    Type: FromType<V2>,
-    Type: FromType<V3>,
-    Type: FromType<V4>,
-    V0: Copy + TryFrom<O, Error = &'static str>,
-    V1: Copy + TryFrom<O, Error = &'static str>,
-    V2: Copy + TryFrom<O, Error = &'static str>,
-    V3: Copy + TryFrom<O, Error = &'static str>,
-    V4: Copy + TryFrom<O, Error = &'static str>,
-{
-    type Output = (V0, V1, V2, V3, V4);
-    fn into_vars(&self) -> Vec<Var> {
-        vec![
-            self.0.into(),
-            self.1.into(),
-            self.2.into(),
-            self.3.into(),
-            self.4.into(),
-        ]
-    }
-
-    fn into_tuple_args(&self, bindings: Vec<O>) -> Self::Output {
-        assert!(bindings.len() == 5);
-
-        (
-            V0::try_from(bindings[0].clone()).unwrap(),
-            V1::try_from(bindings[1].clone()).unwrap(),
-            V2::try_from(bindings[2].clone()).unwrap(),
-            V3::try_from(bindings[3].clone()).unwrap(),
-            V4::try_from(bindings[4].clone()).unwrap(),
-        )
-    }
-}
+impl_var_ref_tuple!();
+impl_var_ref_tuple!(0);
+impl_var_ref_tuple!(0, 1);
+impl_var_ref_tuple!(0, 1, 2);
+impl_var_ref_tuple!(0, 1, 2, 3);
+impl_var_ref_tuple!(0, 1, 2, 3, 4);
+impl_var_ref_tuple!(0, 1, 2, 3, 4, 5);
+impl_var_ref_tuple!(0, 1, 2, 3, 4, 5, 6);
+impl_var_ref_tuple!(0, 1, 2, 3, 4, 5, 6, 7);
