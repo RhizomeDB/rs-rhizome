141258bf22d1adfc38aa81f1b6723ebb6cbde541
Quinn Wilton <quinn@quinnwilton.com>
2023-04-22 19:47:32 -0400

    chore: add placeholder benchmark to pass CI

--- rhizome-benches/benches/a_benchmark.rs
+++ rhizome-benches/benches/a_benchmark.rs
@@ -1,6 +1,8 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn add_benchmark(_c: &mut Criterion) {}
+pub fn add_benchmark(c: &mut Criterion) {
+    c.bench_function("add", |b| b.iter(|| 1 + 2));
+}
 
 criterion_group!(benches, add_benchmark);
 criterion_main!(benches);
