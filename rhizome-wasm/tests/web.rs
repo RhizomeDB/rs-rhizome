acb85dbbabc8f57080c4a274a73cec1f8f8a866c
Quinn Wilton <quinn@quinnwilton.com>
2023-05-02 13:57:18 -0700

    refactor: remove unused imports

--- rhizome-wasm/tests/web.rs
+++ rhizome-wasm/tests/web.rs
@@ -2,6 +2,6 @@
 
 //! Test suite for the Web and headless browsers.
 
-use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
+use wasm_bindgen_test::{wasm_bindgen_test_configure};
 
 wasm_bindgen_test_configure!(run_in_browser);
